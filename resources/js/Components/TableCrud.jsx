13b956460550f9caeaaacb49a7323b5349aae478
saulmonci <saul.armando667@gmail.com>
2026-06-22 23:39:02 -0600

    feat: set search component layout to vertical in TableCrud

--- resources/js/Components/TableCrud.jsx
+++ resources/js/Components/TableCrud.jsx
@@ -172,6 +172,7 @@ export default function TableCrud({
             }
         },
         search: search ? { 
+            layout: 'vertical',
             defaultCollapsed: false, 
             ...search
         } : false,
